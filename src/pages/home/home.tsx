a3db62ea6da4c3e83976a27c69c83406ce7191c1
Zertonixa <olpin9070@gmail.com>
2025-11-17 06:14:58 +0300

    feat(home): add home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,3 +1,20 @@
+import { Booking } from "@widgets/booking/booking";
+import { Header } from "@widgets/header/header";
+import { RedirectMenu } from "@widgets/redirectMenu/redirectMenu";
+import { NearestSlots } from "@/widgets/nearestSlots";
+import styles from "./home.module.scss";
+
 export const Home = () => {
-  return <div>Its a home page</div>;
+  return (
+    <div className={styles.container}>
+      <Header title="Доброе утро, User!" />
+      <div className={styles.containerWrap}>
+        <div className={styles.containerWrapLeft}>
+          <RedirectMenu />
+          <Booking />
+        </div>
+        <NearestSlots />
+      </div>
+    </div>
+  );
 };
